d8a59737d137fd614f7955e896f2ac4294dd49a6
Kevin Wang <wangkevin448@gmail.com>
2016-08-03 20:27:34 -0700

    change dir structure for modularity

---
+++ app.js
@@ -0,0 +1,2 @@
+const app = require('./lib').default.app;
+app.listen(3000, () => console.log('server launched on localhost:3000'));
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = new Koa();
 
 // response
 app.use(async (ctx) => {
-  ctx.body = 'Hello World';
+  ctx.body = 'Hello World!';
 });
 
-app.listen(3000, () => console.log('server launched on localhost:3000'));
+export default {app};
