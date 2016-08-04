c711112dd3dfa489f31652df29506a2ea0e9a06c
Kevin Wang <wangkevin448@gmail.com>
2016-08-04 01:17:20 -0700

    configure middleware and routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import 'babel-polyfill';
 import Koa from 'koa';
-const app = new Koa();
+import Router from 'koa-router';
 
-// response
-app.use(async (ctx) => {
+const app = new Koa();
+const router = new Router();
+
+const requestTimeLogger = async (ctx, next)=>{
+  const timeStart = new Date();
+  await next();
+  const timeElapsed = Date.now() - timeStart.getTime();
+  console.log(`request at ${timeStart}, time elapsed: ${timeElapsed}ms`);
+};
+
+const helloWorld = async (ctx, next)=>{
   ctx.body = 'Hello World!';
-});
+  await next();
+};
+
+router
+  .get('/', helloWorld);
+
+app
+  .use(requestTimeLogger)
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 export {app};
