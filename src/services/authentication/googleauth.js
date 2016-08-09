454d6148e388bedb4b155d06f2ce7e9fd04a4676
Kevin Wang <wangkevin448@gmail.com>
2016-08-09 15:30:50 -0700

    prepare for passport

--- src/services/authentication/googleauth.js
+++ src/services/authentication/googleauth.js
@@ -1,5 +1,3 @@
-import JWT from 'jsonwebtoken';
-import fetch from 'node-fetch';
 import {Authentication} from './auth';
 
 class GoogleAuth extends Authentication {
@@ -8,22 +6,7 @@ class GoogleAuth extends Authentication {
     this.clientSecret_ = clientSecret;
   }
 
-  async getGoogleCerts(KId, box){
-    fetch(this.clientSecret_.auth_provider_x509_cert_url)
-      .then((res)=>{
-        return res.json();
-      }).then((json)=>{
-        const key = json[KId];
-      }).catch(function(ex) {
-        const exception = ex;
-      });
-  }
-
   verify(token){
-    const decodedToken = JWT.decode(token, {complete: true, json: true});
-    if(!decodedToken){
-      return [false, 'Error: invalid token'];
-    }
 
   }
 }
