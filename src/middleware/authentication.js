9084ff3d02568038e346a1bd16b64ba5972497fb
Kevin Wang <wangkevin448@gmail.com>
2016-08-10 23:39:55 -0700

    support multiple tiers in authenticator

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,14 +1,18 @@
-const Authenticator = (permissionTier, user)=>{
+const Authenticator = (user, permissionTiers)=>{
+  /**
+  user - username of allowed user
+  permissionTiers - array of accepted tiers from most common to least
+  */
   return async (ctx, next)=>{
     const {authentication, repository} = ctx.services;
     const {username, idToken} = ctx.request.body;
     const {googleId} = await authentication.verify(idToken);
     const profile = await repository.retrieve(username);
-    if(profile.googleId == googleId){
-      // if request's user is a part of the permissionTier or the user is granted access because of ownership
-      if(permissionTier && profile.tags.indexOf(permissionTier) || user && user == profile.username){
-        await next();
-      }
+    if( (profile.googleId == googleId) && ((user && user == profile.username) || (permissionTiers && permissionTiers.filter((tier)=>{
+      return profile.tags.indexOf(tier) > -1;
+    }).length > 0)) ){
+      // if request's user is granted access because of ownership or user is a part of the permissionTiers
+      await next();
     }
   };
 };
